46141521d247b38d41ff5605adb277fb5d00bfbb
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-11-24 21:38:32 -0500

    Update tkinter_functions.py

--- tkinter_functions.py
+++ tkinter_functions.py
@@ -1,1 +1,44 @@
 from tkinter import *
+
+
+
+root = Tk()
+
+
+def my_click():
+    pass
+    
+    
+
+my_button = Button(root, text = "Agriculture", command= my_click, padx= 40, pady=1)
+my_button.grid(row = 0, column= 0)
+
+my_button1 = Button(root, text = "FFMQOG", command= my_click, padx= 40, pady=1)
+my_button1.grid(row = 0, column= 1)
+
+my_button2 = Button(root, text = "Construction", command= my_click, padx= 40, pady=1)
+my_button2.grid(row = 0, column= 2)
+
+my_button3 = Button(root, text = "Wholesale and retail", command= my_click, padx= 40, pady=1)
+my_button3.grid(row = 0, column= 3)
+
+my_button4 = Button(root, text = "Transportation", command= my_click, padx= 40, pady=1)
+my_button4.grid(row = 0, column= 4)
+
+my_button5 = Button(root, text = "Education", command= my_click, padx= 40, pady=1)
+my_button5.grid(row = 0, column= 5)
+
+my_button6 = Button(root, text = "HC & SA", command= my_click, padx= 40, pady=1)
+my_button6.grid(row = 0, column= 6)
+
+my_button7 = Button(root, text = "A & Food Services", command= my_click, padx= 40, pady=1)
+my_button7.grid(row = 0, column= 7)
+
+my_button8 = Button(root, text = "Public administration", command= my_click, padx= 40, pady=1)
+my_button8.grid(row = 0, column= 8)
+
+
+    
+
+
+root.mainloop()                                
