e788101ccad207fdcf4ee45d69b3420e2a0fc062
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-11-27 19:02:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import project_part_1 as proj1
+import project_part_2 as proj2
 
 root = Tk()
 
