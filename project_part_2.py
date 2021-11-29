5b2dd31b675218a9887f0bf684b12a5ed7130637
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-11-28 22:44:26 -0500

    Update project_part_2.py

---
+++ project_part_2.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plot
+import pandas as p
+
+during_pandemic_data = p.read_csv('Datasets/during_the_pandemic (1).csv')
+pre_pandemic_data = p.read_csv('Datasets/pre_pandemic.csv')
