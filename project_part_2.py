a98aa1b8a5b73187de07b8e6e5f1fb4286ee3517
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-12-02 19:52:52 -0500

    Update project_part_2.py

--- project_part_2.py
+++ project_part_2.py
@@ -6,13 +6,12 @@ from datetime import time
 import matplotlib.pyplot as plot
 import pandas as p
 import project_part_1 as proj1
-industries_and_indexes = proj1.industry_and_its_index
+industries_and_indexes = proj1.industry_and_its_index      # The dictionary corresponding to the industries
 
 
 pre_pandemic_data = p.read_csv('Datasets/pre_pandemic.csv')
 during_the_pandemic_data = p.read_csv('Datasets/during_the_pandemic (1).csv')
-print(pre_pandemic_data)
-print(pre_pandemic_data.iloc[0, 1])
+
 
 def create_dataframe(industry: str, time_frame: str) -> p.DataFrame:
     """
@@ -27,8 +26,10 @@ def create_dataframe(industry: str, time_frame: str) -> p.DataFrame:
         - industry in proj1.industry_and_its_index
     """
     #Creating a new DataFrame where rows and columns are 0-indexed by default
-    new_dataframe = p.DataFrame(columns= ["industry", "timeframe",  "Increase- Decrease", "Percentage of Increase - Decrease"], index= ["values"])
+    new_dataframe = p.DataFrame(columns= ["industry", "timeframe", "pre-pandemic value","value during the pandemic" ,  "Increase- Decrease", "Percentage of Increase - Decrease"], index= ["values"])
     new_dataframe["industry"] = industry
+    
+    
     dates_to_indexes = {"January 2019- 20": 0, "February 2019- 20": 1,
                         "March 2019- 20": 2, "April 2019- 20": 4,
                         "May 2019- 20": 3, "June 2019- 20": 5,
@@ -44,15 +45,22 @@ def create_dataframe(industry: str, time_frame: str) -> p.DataFrame:
     # For example iloc[4 : 7, 3 : 14] would return the rows 4, 5 and 6; the columns from 3 up to but not including 14.
     
     
-    # Considering each case separately.
-
-    new_dataframe["timeframe"] = dates[dates_to_indexes[time_frame]]
+    new_dataframe["timeframe"] = dates[dates_to_indexes[time_frame]] #Modifying the "timeframe" column
+    
     value_1 = during_the_pandemic_data.iloc[industries_and_indexes[industry], dates_to_indexes[time_frame] + 1]
     # Getting the necessary value from during_the_pandemic_data
     value_2 = pre_pandemic_data.iloc[industries_and_indexes[industry], dates_to_indexes[time_frame] + 1]
     # Getting the necessary value from pre_pandemic_data
+    
+    new_dataframe["pre-pandemic value"] = value_2
+    new_dataframe["value during the pandemic"] = value_1
+    
     new_dataframe["Increase- Decrease"] = value_1 - value_2
+    
     new_dataframe["Percentage of Increase - Decrease"] = 100 * ((value_1 - value_2) / value_2)
     # Getting a percentage
 
     return new_dataframe
+
+
+print(create_dataframe("Public administration", "June 2019- 20"))
