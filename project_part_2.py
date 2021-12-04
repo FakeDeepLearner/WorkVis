96f03ba103e8155926390e277f68ffafff7f62ff
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-12-04 18:15:03 -0500

    Update project_part_2.py

--- project_part_2.py
+++ project_part_2.py
@@ -1,11 +1,64 @@
 """
-This module contains all of the code needed to implement the second part of the project.
-It must be imported into the main.py file
+This module contains the code required to operate the second part of the project
 """
 
+from datetime import time
 import matplotlib.pyplot as plot
 import pandas as p
+import project_part_1 as proj1
+industries_and_indexes = proj1.industry_and_its_index      # The dictionary corresponding to the industries
 
-during_pandemic_data = p.read_csv('Datasets/during_the_pandemic (1).csv')
+
 pre_pandemic_data = p.read_csv('Datasets/pre_pandemic.csv')
+during_the_pandemic_data = p.read_csv('Datasets/during_the_pandemic (1).csv')
+
+
+def create_dataframe(industry: str, time_frame: str) -> p.DataFrame:
+    """
+    Create and return a new pandas dataframe
+    
+    Preconditions:
+        - timeframe in ["January 2019- 20", "February 2019- 20", "March 2019- 20", 
+                "April 2019- 20", "May 2019- 20", "June 2019- 20", 
+               "July 2019- 20", "August 2019- 20", "September 2019- 20", 
+                "October 2019- 20", "November 2019- 20", "Decemeber 2019- 20"]
+                
+        - industry in proj1.industry_and_its_index
+    """
+    #Creating a new DataFrame where rows and columns are 0-indexed by default
+    new_dataframe = p.DataFrame(columns= ["industry", "timeframe", "pre-pandemic value","value during the pandemic" ,  "Increase- Decrease", "Percentage of Increase - Decrease"], index= ["values"])
+    new_dataframe["industry"] = industry
+    
+    
+    dates_to_indexes = {"January 2019- 20": 0, "February 2019- 20": 1,
+                        "March 2019- 20": 2, "April 2019- 20": 3,
+                        "May 2019- 20": 4, "June 2019- 20": 5,
+                        "July 2019- 20": 6, "August 2019- 20": 7,
+                        "September 2019- 20": 8, "October 2019- 20": 9,
+                        "November 2019- 20": 10, "Decemeber 2019- 20": 11}
+    dates = list(dates_to_indexes.keys())
+
+
+    # The iloc[:, :] function allows us to access specific values from the DataFrame by their indexes. 
+    # The first parameter, which is the rows,  takes 2 integers separated by a colon (:) and gives out that part of the data frame. The end point is exclusive.
+    # The second parameter, which is the colums,  takes 2 integers separated by a colon (:) and gives out that part of the data frame. The end point is exclusive.
+    # For example iloc[4 : 7, 3 : 14] would return the rows 4, 5 and 6; the columns from 3 up to but not including 14.
+    
+    
+    new_dataframe["timeframe"] = dates[dates_to_indexes[time_frame]] #Modifying the "timeframe" column
+    
+    value_1 = during_the_pandemic_data.iloc[industries_and_indexes[industry], dates_to_indexes[time_frame] + 1]
+    # Getting the necessary value from during_the_pandemic_data
+    value_2 = pre_pandemic_data.iloc[industries_and_indexes[industry], dates_to_indexes[time_frame] + 1]
+    # Getting the necessary value from pre_pandemic_data
+    
+    new_dataframe["pre-pandemic value"] = value_2
+    new_dataframe["value during the pandemic"] = value_1
+    
+    new_dataframe["Increase- Decrease"] = value_1 - value_2
+    
+    new_dataframe["Percentage of Increase - Decrease"] = 100 * ((value_1 - value_2) / value_2)
+    # Getting a percentage
+
+    return new_dataframe
 
