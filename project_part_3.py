44eaca8b5b55d42ee833240e1d480ed17902cb82
Stalight_ <fishy754@gmail.com>
2021-12-11 21:16:41 -0500

    Update project_part_3.py

--- project_part_3.py
+++ project_part_3.py
@@ -23,7 +23,8 @@ def create_dataframe(industry: str) -> p.DataFrame:
     Preconditions:
         - industry in proj1.industry_and_its_index
     """
-    new_dataframe = p.DataFrame(columns= ["Industry", "Pre-Pandemic Average", "Average During the Pandemic"], index= ["values"])
+    new_dataframe = p.DataFrame(columns=["Industry", "Pre-Pandemic Average",
+                                         "Average During the Pandemic"], index=["values"])
 
     industry_name = industry.split(' ')
     industry_name_fixed = [industry_name[0]]
@@ -51,12 +52,13 @@ def create_dataframe(industry: str) -> p.DataFrame:
     during_the_pandemic_total = 0
     
     # Accumulating the sum of the necessary row, decided by the industry passed in.
-    for col_num in range(1, 13): # There are a total of 12 rows, representing each month
+    for col_num in range(1, 13):
+        # There are a total of 12 rows, representing each month
         pre_pandemic_total += float(pre_pandemic_data.iloc[industries_and_indexes[industry], col_num])
         during_the_pandemic_total += float(during_the_pandemic_data.iloc[industries_and_indexes[industry], col_num])
     
-    new_dataframe["Pre-Pandemic Average"] = pre_pandemic_total / 12
-    new_dataframe["Average During the Pandemic"] = during_the_pandemic_total / 12
+    new_dataframe["Pre-Pandemic Average"] = round(pre_pandemic_total / 12, 2)
+    new_dataframe["Average During the Pandemic"] = round(during_the_pandemic_total / 12, 2)
 
     return new_dataframe
     
