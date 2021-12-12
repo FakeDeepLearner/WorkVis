59f0fe9a5102a588cc2ad1148bfa0ca5be99043a
Stalight_ <fishy754@gmail.com>
2021-12-11 21:17:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -220,78 +220,78 @@ root.mainloop()
 #########
 # Part 3
 #########
-
-root = Tk()
-root.overrideredirect(True)
-root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-root.config(bg='#73C2FB')
-
-frame = Frame(root)
-frame.pack()
-frame.config(bg='#73C2FB')
-
-bottomframe = Frame(root)
-bottomframe.pack(side=LEFT)
-
-canvas = None
-
-button_of_agriculture = Button(frame, text="Agriculture",
-                               command=lambda: display_dataframe("Agriculture"))
-button_of_agriculture.grid(row=0, column=0)     # Placing the buttons on the screen
-
-button_of_FFMQOG = Button(frame, text="Forestry, Fishing, Mining, Quarrying, Oil and Gas",
-                          command=lambda: display_dataframe("Forestry, fishing, mining, quarrying, oil and gas"))
-button_of_FFMQOG.grid(row=0, column=1)
-
-button_of_construction = Button(frame, text="Construction",
-                                command=lambda: display_dataframe("Construction"))
-button_of_construction.grid(row=0, column=2)
-
-button_of_wholesale = Button(frame, text="Wholesale and Retail Trade",
-                             command=lambda: display_dataframe("Wholesale and retail trade"))
-button_of_wholesale.grid(row=0, column=3)
-
-button_of_transportation_and_warehousing = Button(frame, text="Transportation and Warehousing",
-                                                  command=lambda: display_dataframe("Transportation and warehousing"))
-button_of_transportation_and_warehousing.grid(row=0, column=4)
-
-button_of_edicational_services = Button(frame, text="Education",
-                                        command=lambda: display_dataframe("Educational services"))
-button_of_edicational_services.grid(row=0, column=5)
-
-button_of_health_care = Button(frame, text="Health Care and Social Assistance",
-                               command=lambda: display_dataframe("Health care and social assistance"))
-button_of_health_care.grid(row=0, column=6)
-
-button_of_accomodation_and_food = Button(frame, text="Accommodation and Food Services",
-                                         command=lambda: display_dataframe("Accommodation and food services"))
-button_of_accomodation_and_food.grid(row=0, column=7)
-
-button_of_public_administration = Button(frame, text="Public Administration",
-                                         command=lambda: display_dataframe("Public administration"))
-button_of_public_administration.grid(row=0, column=8)
-
-quit_button = Button(frame, text="Close the program", command=lambda: root.destroy(), bg='light yellow')
-quit_button.grid(row=3, column=4)
-# The quit button
-
-
-def display_dataframe(industry: str) -> None:
-    """
-    Display the DataFrame to the tkinter window, depending on which button was pressed
-    """
-    global canvas, bottomframe
-
-    if canvas:  # Removing the existing graph (if there is any)
-        bottomframe.destroy()
-        bottomframe = Frame(root)
-        bottomframe.pack(side=BOTTOM)
-
-    # Generating the figure and a canvas to display it
-    figure = proj3.create_table(industry)
-    canvas = FigureCanvasTkAgg(figure, master=bottomframe)
-
-    canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)  # Displaying the figure
-
-
-root.mainloop()
+#
+# root = Tk()
+# root.overrideredirect(True)
+# root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+# root.config(bg='#73C2FB')
+#
+# frame = Frame(root)
+# frame.pack()
+# frame.config(bg='#73C2FB')
+#
+# bottomframe = Frame(root)
+# bottomframe.pack(side=LEFT)
+#
+# canvas = None
+#
+# button_of_agriculture = Button(frame, text="Agriculture",
+#                                command=lambda: display_dataframe("Agriculture"))
+# button_of_agriculture.grid(row=0, column=0)     # Placing the buttons on the screen
+#
+# button_of_FFMQOG = Button(frame, text="Forestry, Fishing, Mining, Quarrying, Oil and Gas",
+#                           command=lambda: display_dataframe("Forestry, fishing, mining, quarrying, oil and gas"))
+# button_of_FFMQOG.grid(row=0, column=1)
+#
+# button_of_construction = Button(frame, text="Construction",
+#                                 command=lambda: display_dataframe("Construction"))
+# button_of_construction.grid(row=0, column=2)
+#
+# button_of_wholesale = Button(frame, text="Wholesale and Retail Trade",
+#                              command=lambda: display_dataframe("Wholesale and retail trade"))
+# button_of_wholesale.grid(row=0, column=3)
+#
+# button_of_transportation_and_warehousing = Button(frame, text="Transportation and Warehousing",
+#                                                   command=lambda: display_dataframe("Transportation and warehousing"))
+# button_of_transportation_and_warehousing.grid(row=0, column=4)
+#
+# button_of_edicational_services = Button(frame, text="Education",
+#                                         command=lambda: display_dataframe("Educational services"))
+# button_of_edicational_services.grid(row=0, column=5)
+#
+# button_of_health_care = Button(frame, text="Health Care and Social Assistance",
+#                                command=lambda: display_dataframe("Health care and social assistance"))
+# button_of_health_care.grid(row=0, column=6)
+#
+# button_of_accomodation_and_food = Button(frame, text="Accommodation and Food Services",
+#                                          command=lambda: display_dataframe("Accommodation and food services"))
+# button_of_accomodation_and_food.grid(row=0, column=7)
+#
+# button_of_public_administration = Button(frame, text="Public Administration",
+#                                          command=lambda: display_dataframe("Public administration"))
+# button_of_public_administration.grid(row=0, column=8)
+#
+# quit_button = Button(frame, text="Close the program", command=lambda: root.destroy(), bg='light yellow')
+# quit_button.grid(row=3, column=4)
+# # The quit button
+#
+#
+# def display_dataframe(industry: str) -> None:
+#     """
+#     Display the DataFrame to the tkinter window, depending on which button was pressed
+#     """
+#     global canvas, bottomframe
+#
+#     if canvas:  # Removing the existing graph (if there is any)
+#         bottomframe.destroy()
+#         bottomframe = Frame(root)
+#         bottomframe.pack(side=BOTTOM)
+#
+#     # Generating the figure and a canvas to display it
+#     figure = proj3.create_table(industry)
+#     canvas = FigureCanvasTkAgg(figure, master=bottomframe)
+#
+#     canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)  # Displaying the figure
+#
+#
+# root.mainloop()
